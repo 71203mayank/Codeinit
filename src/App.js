330b6f22ab81f0d18098378657ef457997cb3f7a
Jayy1287 <sanjayrajakumar17@gmail.com>
2023-02-04 16:13:11 +0530

    Commit 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Left from './Components/Left/Left';
 import Login from './Components/LoginPage/Login';
 import MainPage from './Components/MainPage/MainPage';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Login/>
       <MainPage/>
       <Left/>
--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import './navBar.css';
+import './NavBar.css';
+import nitc from './nitc.png';
+import lostnfound from './lostnfound.png';
 
 function navBar(){
   return(
     <div className='NavBar'>
         <div className='nitcLogo'>
+            <img src= {nitc} alt="Loading" />
 
         </div>
         <div className='NavHead'>
-
+        
         </div>
+        <div className = 'LnFLogo'> 
+          <img src={lostnfound} alt="Loading" />
+        </div>
+
     </div>
   )
 }
