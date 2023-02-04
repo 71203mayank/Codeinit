be59a6feeb68e4473590dc51e33f030a8476cead
FendrelCabrera <shalin36shayne@gmail.com>
2023-02-05 03:36:17 +0530

    Api merge

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -5,21 +5,21 @@ const url = "https://Codeinit.avinashkumar35.repl.co/";
 const getAllItems = async () => {
   const endpoint = "all";
 
-  data = await axios.get(`${url}/${endpoint}`);
+  let data = await axios.get(`${url}/${endpoint}`);
   console.log(data);
   return;
 };
 
 const getUserAds = async (userEmail) => {
   const endpoint = "user";
-  data = await axios.post(`${url}/${endpoint}`, { email: userEmail });
+  let data = await axios.post(`${url}/${endpoint}`, { email: userEmail });
   console.log(data);
   return;
 };
 
 const getRecent = async () => {
   const endpoint = "recent";
-  data = await axios.get(`${url}/${endpoint}`);
+  let data = await axios.get(`${url}/${endpoint}`);
 };
 
 export { getAllItems, getRecent, getUserAds };
